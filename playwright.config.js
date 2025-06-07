04b15ea72ee0000ab3374e2b76e06eed6d993b0b
Deepti Wani <142917253+DeeptiWani@users.noreply.github.com>
2025-06-07 22:24:38 +0530

    Create playwright.config.js

---
+++ playwright.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  timeout: 30000,
+  expect: {
+    timeout: 5000,
+  },
+  reporter: [['html', { open: 'never' }]],
+  use: {
+    headless: true,
+    viewport: { width: 1280, height: 720 },
+    ignoreHTTPSErrors: true,
+    video: 'retain-on-failure',
+    baseURL: 'https://opensource-demo.orangehrmlive.com',
+    screenshot: 'only-on-failure',
+    trace: 'on-first-retry',
+  },
+});
